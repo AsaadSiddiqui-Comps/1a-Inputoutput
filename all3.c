7ac0d9b2634908aeaf20accd94c4b5483a42da58
AsaadSiddiqui-Comps <asaad.241258.co@mhssce.ac.in>
2024-11-27 20:36:21 +0530

    Add files via upload

---
+++ all3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+int roll_no;
+
+//Enter Your String Code
+char str[100000];
+printf("Enter any Number from 1 to 99999 \n");
+gets(str);
+
+
+//Enter Your Character Here code
+char ch;
+printf("enter your character \n");
+ch=getchar();
+
+
+
+//Enter Your Roll no. Here code
+printf("Enter your roll number \n");
+scanf("%d" , & roll_no);
+
+
+
+printf("Your String is: \n");
+puts(str);
+printf("you have entered: \n");
+putchar (ch);
+printf("Your Roll No. is %d", roll_no);
+
+return(0);
+
+}
