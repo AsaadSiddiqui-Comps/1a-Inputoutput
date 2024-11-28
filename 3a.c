c91a5785befca6bc135dccf958daf4e1784dfffe
AsaadSiddiqui-Comps <asaad.241258.co@mhssce.ac.in>
2024-11-28 20:37:38 +0530

    Update 3a.c

--- 3a.c
+++ 3a.c
@@ -4,51 +4,67 @@
 /* PROGRAM NO 1-B
 /*program to demonstrate nested control structure */
 #include <stdio.h>
-int main(){
-int num, day;
-//if else loop nested (number is positive and number is even)
-printf("Enter Your Number \n");
-scanf("%d", &num);
-if(num>=0)
-{
-    printf("you number is positive \n");
-  if(num%2==0)
-    printf("Your Number Is Even \n");
-   else
-     printf("Your Number Is odd \n");
-}
-else
-    printf("Number is Negative \n");
-//Switch (char "day" monday)
-printf("Enter day (1-7) for days of the week \n");
+
+int main() {
+
+    int num, day;
+
+    // If-Else Loop Nested (Check if the number is positive and even/odd)
+    printf("Enter Your Number: \n");
+    scanf("%d", &num);
+
+    if (num >= 0) {
+        printf("Your number is positive.\n");
+        if (num % 2 == 0)
+            printf("Your number is even.\n");
+        else
+            printf("Your number is odd.\n");
+    } else {
+        printf("Your number is negative.\n");
+    }
+
+    // Switch Case (Days of the week)
+    printf("Enter day (1-7) for days of the week: \n");
     scanf("%d", &day);
-    switch(day) {
+
+    switch (day) {
         case 1:
-            printf("Mon\n");
+            printf("Monday\n");
             break;
         case 2:
-            printf("Tue\n");
+            printf("Tuesday\n");
             break;
         case 3:
-            printf("Wed\n");
+            printf("Wednesday\n");
             break;
         case 4:
-            printf("Thu\n");
+            printf("Thursday\n");
             break;
         case 5:
-            printf("Fri\n");
+            printf("Friday\n");
             break;
         case 6:
-            printf("Sat\n");
+            printf("Saturday\n");
             break;
         case 7:
-            printf("Sun\n");
+            printf("Sunday\n");
             break;
         default:
-            printf("Invalid input! Please enter a number between 1 and 7.\n");
+            printf("Invalid number! Please enter a number between 1 and 7.\n");
             break;
     }
-//using Nested (for loop for different pattern)
 
-return(0);
-} 
+    // Nested For Loop (Pattern Printing)
+    int n;
+    printf("Enter the number (pattern): \n");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {  
+        for (int j = 1; j <= i; j++) {  
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
