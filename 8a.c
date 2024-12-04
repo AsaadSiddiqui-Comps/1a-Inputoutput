7eb2ab8fac21f5a5a7f26510e5482f496b67f8c4
AsaadSiddiqui-Comps <asaad.241258.co@mhssce.ac.in>
2024-12-04 21:43:45 +0530

    Create 8a.c

---
+++ 8a.c
@@ -0,0 +1,13 @@
+/* NAME: Siddiqui Mohammed Asaad Shakir
+/*Roll no: 241258
+/*Branch:computer engineering   Batch: B3
+
+#include <stdio.h>
+int main()
+{
+ char name[20];
+ printf("Enter name: ");
+ scanf("%s", name);
+ printf("Your name is %s.", name);
+ return 0;
+}
